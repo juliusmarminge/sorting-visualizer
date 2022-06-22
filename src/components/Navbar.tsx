35354b5d455fc90d356f3531c87b7d91c9c7ee36
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-06-22 12:53:23 +0200

    remove font-awesome dependency

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,48 @@
 import React, { Dispatch, SetStateAction } from "react";
 import { useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faRedoAlt, faPlayCircle } from "@fortawesome/free-solid-svg-icons";
 import { config } from "../utils/config";
 import type { Algorithm } from "../utils/algorithms";
 
 const { algorithms } = config;
+
+const RefreshIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="h-6 w-6"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+    stroke-width="2"
+  >
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
+    />
+  </svg>
+);
+
+const PlayIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="h-6 w-6"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+    stroke-width="2"
+  >
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"
+    />
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+    />
+  </svg>
+);
 
 export const Navbar: React.FC<{
   startVisualization: (algorithm: Algorithm) => void;
@@ -96,7 +133,7 @@ export const Navbar: React.FC<{
             onClick={generateArray}
             disabled={isAnimating}
           >
-            <FontAwesomeIcon className="fa-icon" icon={faRedoAlt} />
+            <RefreshIcon />
             New Bars
           </button>
           <button
@@ -104,7 +141,7 @@ export const Navbar: React.FC<{
             onClick={visualize}
             disabled={isAnimating}
           >
-            <FontAwesomeIcon className="fa-icon" icon={faPlayCircle} />
+            <PlayIcon />
             Visualize!
           </button>
         </div>
